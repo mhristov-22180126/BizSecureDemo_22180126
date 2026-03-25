3a9bc5cf6f5eac6e3d7e8549756ab11a3450f6ff
Mihail Hristov <mishohristov3@gmail.com>
2026-03-25 10:28:33 +0200

    create search contoller

---
+++ BizSecureDemo_22180126/Controllers/SearchController.cs
@@ -0,0 +1,28 @@
+﻿using BizSecureDemo_22180126.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+namespace BizSecureDemo.Controllers;
+[Authorize]
+public class SearchController : Controller
+{
+    private readonly AppDbContext _db;
+    public SearchController(AppDbContext db)
+    {
+        _db = db;
+    }
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> Results(string keyword)
+    {
+        var sql = $"SELECT * FROM Orders WHERE Title LIKE '%{keyword}%'";
+        var results = await _db.Orders
+            .FromSqlRaw(sql)
+            .ToListAsync();
+        return View(results);
+    }
+}
